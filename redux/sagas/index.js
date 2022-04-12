b6541f043c1b896ba41792b83b04e5af8206b6fe
BeautyKim <kima2012114@gmail.com>
2022-04-12 10:33:28 +0900

    new branch

--- redux/sagas/index.js
+++ redux/sagas/index.js
@@ -1,6 +1,6 @@
 import { takeLatest, all, put } from 'redux-saga/effects'
 import {} from './user.saga'
 
-export default function* rootSaga(){
-    yield all([fork(watchSignup)])
+export function* rootSaga(){
+    yield all([fork(watchJoin)])
 }
